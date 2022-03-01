0de57bb210651dc583362b1920750a3a09e1d9b0
memoeslink <memocad@gmail.com>
2022-03-01 16:22:35 -0600

    Changed strings.xml and es\strings.xml to change some content dynamically.

--- app/src/main/java/com/app/memoeslink/adivinador/AboutActivity.kt
+++ app/src/main/java/com/app/memoeslink/adivinador/AboutActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.widget.Button
 import android.widget.TextView
+import com.memoeslink.generator.common.DateTimeHelper
 
 class AboutActivity : CommonActivity() {
     private var tvContent: TextView? = null
@@ -14,7 +15,15 @@ class AboutActivity : CommonActivity() {
         setContentView(R.layout.activity_about)
         tvContent = findViewById(R.id.about_content)
         tvContent?.isClickable = true
-        tvContent?.text = TextFormatter.fromHtml(getString(R.string.about))
+        tvContent?.text = TextFormatter.fromHtml(
+            getString(
+                R.string.about,
+                getString(R.string.app_name),
+                BuildConfig.VERSION_NAME,
+                BuildConfig.VERSION_CODE,
+                DateTimeHelper.getCurrentDate().year
+            )
+        )
         tvContent?.movementMethod = LinkMovementMethod.getInstance()
         btBack = findViewById(R.id.about_back_button)
 
