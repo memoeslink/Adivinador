4c2d6690e4d1d305f3482414264e786250dc2cb9
memoeslink <memoeslink@hotmail.com>
2023-05-22 14:18:31 -0600

    Refactored SettingsActivity.java into SettingsActivity.kt. Fixed ads not hiding after setting changes.

--- app/src/main/java/com/app/memoeslink/adivinador/activity/SettingsActivity.kt
+++ app/src/main/java/com/app/memoeslink/adivinador/activity/SettingsActivity.kt
@@ -1,117 +1,95 @@
-package com.app.memoeslink.adivinador.activity;
+package com.app.memoeslink.adivinador.activity
 
-import android.content.SharedPreferences;
-import android.media.AudioManager;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.view.KeyEvent;
+import android.content.DialogInterface
+import android.content.SharedPreferences
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AlertDialog
+import com.app.memoeslink.adivinador.R
+import com.app.memoeslink.adivinador.Screen
+import com.app.memoeslink.adivinador.Speech
+import com.app.memoeslink.adivinador.preference.Preference
+import com.app.memoeslink.adivinador.preference.PreferenceHandler
+import com.takisoft.preferencex.PreferenceFragmentCompat
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
+class SettingsActivity : CommonActivity() {
+    private var dialog: AlertDialog? = null
+    private var listener: OnSharedPreferenceChangeListener? =
+        null //Declared as global to avoid destruction by JVM Garbage Collector
 
-import com.app.memoeslink.adivinador.R;
-import com.app.memoeslink.adivinador.Screen;
-import com.app.memoeslink.adivinador.Speech;
-import com.app.memoeslink.adivinador.preference.Preference;
-import com.app.memoeslink.adivinador.preference.PreferenceHandler;
-import com.takisoft.preferencex.PreferenceFragmentCompat;
-
-public class SettingsActivity extends CommonActivity {
-    private AlertDialog dialog;
-    private AudioManager audioManager;
-    private SharedPreferences.OnSharedPreferenceChangeListener listener; //Declared as global to avoid destruction by JVM Garbage Collector
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, new CustomPreferenceFragment()).commit();
-        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
-        builder.setTitle(getString(R.string.alert_app_restart_title));
-        builder.setMessage(getString(R.string.alert_app_restart_message));
-        builder.setIcon(R.drawable.exit);
-        builder.setNeutralButton(getString(R.string.ok), (dialog, which) -> dialog.dismiss());
-        builder.setOnDismissListener(arg0 -> restartApplication());
-        dialog = builder.create();
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportFragmentManager.beginTransaction()
+            .replace(android.R.id.content, CustomPreferenceFragment()).commit()
+        val builder = AlertDialog.Builder(this@SettingsActivity)
+        builder.setTitle(getString(R.string.alert_app_restart_title))
+        builder.setMessage(getString(R.string.alert_app_restart_message))
+        builder.setIcon(R.drawable.exit)
+        builder.setNeutralButton(getString(R.string.ok)) { dialog: DialogInterface, _: Int -> dialog.dismiss() }
+        builder.setOnDismissListener { restartApplication() }
+        dialog = builder.create()
 
         //Set listeners
-        listener = (prefs, key) -> {
-            if ((key.equals(Preference.SETTING_AUDIO_ENABLED.getTag()) && !PreferenceHandler.getBoolean(Preference.SETTING_AUDIO_ENABLED)) ||
-                    key.equals(Preference.SETTING_VOICE_ENABLED.getTag()) && !PreferenceHandler.getBoolean(Preference.SETTING_VOICE_ENABLED))
-                Speech.getInstance(SettingsActivity.this).suppress();
+        listener = OnSharedPreferenceChangeListener { _: SharedPreferences?, key: String ->
+            if (key == Preference.SETTING_AUDIO_ENABLED.tag && !PreferenceHandler.getBoolean(
+                    Preference.SETTING_AUDIO_ENABLED
+                ) || key == Preference.SETTING_VOICE_ENABLED.tag && !PreferenceHandler.getBoolean(
+                    Preference.SETTING_VOICE_ENABLED
+                )
+            ) Speech.getInstance(this@SettingsActivity).suppress()
 
-            if (key.equals(Preference.SETTING_ACTIVE_SCREEN.getTag()))
-                Screen.setContinuance(SettingsActivity.this, PreferenceHandler.getBoolean(Preference.SETTING_ACTIVE_SCREEN));
+            if (key == Preference.SETTING_ACTIVE_SCREEN.tag) Screen.setContinuance(
+                this@SettingsActivity,
+                PreferenceHandler.getBoolean(Preference.SETTING_ACTIVE_SCREEN)
+            )
 
-            if (key.equals(Preference.SETTING_ADS_ENABLED.getTag()) && PreferenceHandler.getBoolean(Preference.SETTING_ADS_ENABLED))
-                PreferenceHandler.put(Preference.TEMP_RESTART_ACTIVITY, true);
+            if (key == Preference.SETTING_ADS_ENABLED.tag) PreferenceHandler.put(
+                Preference.TEMP_RESTART_ACTIVITY, true
+            )
 
-            if (key.equals(Preference.SETTING_FORTUNE_TELLER_ASPECT.getTag()))
-                PreferenceHandler.put(Preference.TEMP_CHANGE_FORTUNE_TELLER, true);
+            if (key == Preference.SETTING_FORTUNE_TELLER_ASPECT.tag) PreferenceHandler.put(
+                Preference.TEMP_CHANGE_FORTUNE_TELLER, true
+            )
 
-            if (key.equals(Preference.SETTING_LANGUAGE.getTag()) || key.equals(Preference.SETTING_THEME.getTag()) || key.equals(Preference.SETTING_SEED.getTag())) {
+            if (key == Preference.SETTING_LANGUAGE.tag || key == Preference.SETTING_THEME.tag || key == Preference.SETTING_SEED.tag) {
                 try {
-                    SettingsActivity.this.runOnUiThread(() -> dialog.show());
-                } catch (Exception e) {
-                    new Handler(Looper.getMainLooper()).postDelayed(this::restartApplication, 500);
+                    runOnUiThread { dialog?.show() }
+                } catch (e: Exception) {
+                    Handler(Looper.getMainLooper()).postDelayed({ restartApplication() }, 500)
                 }
             }
 
-            if (key.equals(Preference.SETTING_SAVE_NAMES.getTag()) && !PreferenceHandler.getBoolean(Preference.SETTING_SAVE_NAMES, true))
-                PreferenceHandler.remove(Preference.DATA_STORED_NAMES);
+            if (key == Preference.SETTING_SAVE_NAMES.tag && !PreferenceHandler.getBoolean(
+                    Preference.SETTING_SAVE_NAMES, true
+                )
+            ) PreferenceHandler.remove(Preference.DATA_STORED_NAMES)
 
-            if (key.equals(Preference.SETTING_SAVE_ENQUIRIES.getTag()) && !PreferenceHandler.getBoolean(Preference.SETTING_SAVE_ENQUIRIES, true))
-                PreferenceHandler.remove(Preference.DATA_STORED_PEOPLE);
-        };
-        PreferenceHandler.changeDefaultPreferencesListener(listener);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Screen.setContinuance(SettingsActivity.this, PreferenceHandler.getBoolean(Preference.SETTING_ACTIVE_SCREEN));
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        finish();
-        return true;
-    }
-
-    @Override
-    public boolean dispatchKeyEvent(KeyEvent event) {
-        int action = event.getAction();
-        int keyCode = event.getKeyCode();
-
-        switch (keyCode) {
-            case KeyEvent.KEYCODE_VOLUME_UP:
-                if (action == KeyEvent.ACTION_DOWN) {
-                    if (PreferenceHandler.getBoolean(Preference.SETTING_AUDIO_ENABLED))
-                        audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
-                    else
-                        audioManager.adjustStreamVolume(AudioManager.STREAM_RING, AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
-                }
-                return true;
-            case KeyEvent.KEYCODE_VOLUME_DOWN:
-                if (action == KeyEvent.ACTION_DOWN) {
-                    if (PreferenceHandler.getBoolean(Preference.SETTING_AUDIO_ENABLED))
-                        audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
-                    else
-                        audioManager.adjustStreamVolume(AudioManager.STREAM_RING, AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
-                }
-                return true;
-            default:
-                return super.dispatchKeyEvent(event);
+            if (key == Preference.SETTING_SAVE_ENQUIRIES.tag && !PreferenceHandler.getBoolean(
+                    Preference.SETTING_SAVE_ENQUIRIES, true
+                )
+            ) PreferenceHandler.remove(Preference.DATA_STORED_PEOPLE)
         }
+        PreferenceHandler.changeDefaultPreferencesListener(listener)
     }
 
-    public static class CustomPreferenceFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferencesFix(@Nullable Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.default_preferences, rootKey);
+    public override fun onResume() {
+        super.onResume()
+        Screen.setContinuance(
+            this@SettingsActivity, PreferenceHandler.getBoolean(Preference.SETTING_ACTIVE_SCREEN)
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+
+    class CustomPreferenceFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferencesFix(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.default_preferences, rootKey)
         }
     }
 }
