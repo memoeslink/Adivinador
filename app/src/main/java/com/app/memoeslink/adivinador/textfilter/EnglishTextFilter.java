ac52e7834e6a863218347b7721a3b95ea5c06514
memoeslink <memoeslink@hotmail.com>
2023-05-27 15:18:10 -0600

    Added more words to profanity filter.

--- app/src/main/java/com/app/memoeslink/adivinador/textfilter/EnglishTextFilter.java
+++ app/src/main/java/com/app/memoeslink/adivinador/textfilter/EnglishTextFilter.java
@@ -7,19 +7,19 @@ import java.util.regex.Pattern;
 
 public class EnglishTextFilter implements TextFilter {
     public static final String CENSOR_REGEX = "\b" + "ass(es)?|arses?|booty|assh[o0]les?|anus(es)?|anal|arseholes?|rect(ums?|a)|rectal|anilingus|" +
-            "tits?|boobs?|breasts?|busty|(tit|breast|ass)(job|fuck)(ers?|ing)?|titt(y|ies)|bazongas?|nipples?|mammar(y|ies)|tittywanks?|titwanks?|tittiefuckers?|" +
+            "tits?|boobs?|breasts?|bosoms?|busty|(tit|breast|ass)(job|fuck)(ers?|ing)?|titt(y|ies)|bazongas?|nipples?|mammar(y|ies)|tittywanks?|titwanks?|tittiefuckers?|" +
             "sh[i1!]ts?|craps?|golden showers?|(bull|horse|dog)shit|turds?|fae(x|ces)|" +
             "fag(got)?s?|dykes?|sodomites?|lesbos?|trann(y|ies)|siss(y|ies)|cuntboys?|" +
             "niggers?|niggas?|wetbacks?|beaners?|nazis?|crackers?|" +
             "fuck[a-zA-Z]+|fucks?|fuckers?|fucking|fucked|fornicate|fornicators?|fornications?|" +
             "sex|sexual|copulate|copulations?|orgasms?|genitalia|crotch(es)|" +
             "dickheads?|suckers?|scum|scumbags?|dumbfucks?|dumbass(es)?|bitch(es)?|bastards?|bollocks?|goddamn(ed)?|kike|pricks?|twats?|wankers?|buggers?|morons?|retards?|shitheads?|motherfuckers?|motherfuckkas?|motherfucking|motherfuck|" +
-            "dicks?|cocks?|penis(es)?|jizz(es)?|semen|cums?|ejaculations?|ejaculatory|erections?|testicles?|testicular|boners?|choads?|scrot(ums?|a)|cockfoam|dild[o0]s?|ballsacks?|smegma|" +
+            "dicks?|c[o0]cks?|penis(es)?|jizz(es)?|semen|cums?|ejaculations?|ejaculatory|erections?|testicles?|testicular|boners?|choads?|scrot(ums?|a)|cockfoam|dild[o0]s?|ballsacks?|smegma|bukkake|" +
             "vaginas?|vaginal|vag|puss(y|ies)|clitoris(es)?|cunts?|pubes?|pubic|" +
             "blowjobs?|(dick|cock)(lick|suck|eat)(ers?|ing)|fellatio|fellations?|masturbate|masturbations?|masturbatory|masterbate|" +
             "sluts?|hookers?|prostitutes?|whores?|" +
             "kys|" +
-            "pornography|pornographic|p[o0]rn|p[o0]rn[o0]s?|z[o0]{2}philiac?|bestiality|cp|(sado)?masochism|(sado)?masochists?|(sado)?masochistic|" +
+            "pornography|pornographic|p[o0]rn|p[o0]rn[o0]s?|z[o0]{2}philiac?|bestiality|cp|(sado)?masochism|(sado)?masochists?|(sado)?masochistic|sodom(y|ies)|" +
             //"|" +
             //"|" +
             "\b";
--- app/src/main/java/com/app/memoeslink/adivinador/textfilter/SpanishTextFilter.java
+++ app/src/main/java/com/app/memoeslink/adivinador/textfilter/SpanishTextFilter.java
@@ -9,16 +9,16 @@ public class SpanishTextFilter implements TextFilter {
     public static final String CENSOR_REGEX = "(?<start>^|\\s+|[^\\p{IsLatin}])(?<text>" + "sexo|sexual(es)?|coitos?|(coital(es)?)|fornicador([ea]s)?|fornicación|fornicatori[oa]s?|orgasmos?|orgásmic[oa]s?|genital(es)?|copular|cópular|copulatori[oa]s?|follar|follatori[oa]s?|follador(es|as?)?|foll(ón|ones|onas?)|follaculos?|" +
             "t[e3]t[a4]s?|pechos?|senos?|tetamen|tet(ón|ones|onas?)|tetudas?|pez(ón|ones)|tet(ita|ota)s?|pech(ito|ote)s|chich(ita|ota)s?|chich(ón|ones|onas?)|mamar|mamas?|mamari[oa]s?|" +
             "vaginas?|vaginal(es)?|clítoris|conchud[oa]s?|coños?|coñazos?|panochas?|panoch(ita|ota)s?|panochud[oa]s?|panoch(ón|ones|onas?)|pubis|púbic[oa]s?|" +
-            "p[e3]n[e3]s?|vergas?|verg(uita|ota)s?|vergud[oa]s?|verg(ón|ones)|verguer[oa]s?|vergazos?|pijas?|pijud[oa]s?|testículos?|escrotos?|próstatas?|prostátic[oa]s?|erecci(ón|ones)|pijud[0a]s?|dildos?|prepucios?|esmegma|" +
+            "p[e3]n[e3]s?|vergas?|verg(uita|ota)s?|vergud[oa]s?|verg(ón|ones)|verguer[oa]s?|vergazos?|pijas?|pijud[oa]s?|testículos?|escrotos?|próstatas?|prostátic[oa]s?|erecci(ón|ones)|pijud[0a]s?|dildos?|prepucios?|esmegma|chotas?|bukkake|" +
             "nalgas?|nalgonas?|nalg(uita|ota)s?|cul[o0]s?|cul(ón|ones|onas?)|cul(ito|ote)s?|anos?|anal(es)?|rectal(es)?|pedorr[oa]s|pedorrot[oe]s?|" +
-            "mierdas?|mierder[oa]s?|mierd(ón|ones|onas?)|cagadas?|cag(ón|ones|onas?)|cacas?|comemierdas?|he(z|ces)|moj(ón|ones)" +
-            "semen|eyaculaci(ón|ones)|eyaculatori[oa]s?|masturbaci(ón|ones)|masturbatori[oa]s?|onanismos?|puñetas?|puñeter[oa]s?|chaqueter[oa]s?|pajer[oa]s?|" +
+            "mierdas?|mierder[oa]s?|mierd(ón|ones|onas?)|cagadas?|cag(ón|ones|onas?)|cacas?|comemierdas?|he(z|ces)|moj(ón|ones)|" +
+            "semen|eyacular|eyaculaci(ón|ones)|eyaculatori[oa]s?|masturbaci(ón|ones)|masturbatori[oa]s?|onanismos?|puñetas?|puñeter[oa]s?|chaqueter[oa]s?|pajer[oa]s?|" +
             "malnacid[oa]s?|bastard[oa]s?|pendej[oa]s?|putear|put[oa]s?|puterías?|puteros?|putadas?|putazos?|cabr(ón|onas?|ones)|cabronaz[oa]s?|chingad[oa]s?|jodid[oa]s?|joder|jodederas?|" +
-            "(lame|chupa|mama|muerde)(culo|pija|huevo|bola|verga|polla|pene|escroto|prepucio)s?|carevergas?|caraculos?|" +
+            "(lame|chupa|mama|muerde)(culo|pija|huevo|bola|verga|polla|pene|escroto|prepucio|nalga|chota)s?|carevergas?|caraculos?|" +
             "culer[oa]s?|chingar|chingaderas?|chingader(ita|ota)s?|chingonerías?|ching(ón|ones|onas?)|emputad[oa]s?|encabronad[oa]s?|güey(es)?|mam(ón|ones|onas?)|mamadas?|putizas?|pendejear|mamader[oa]s?|put(ón|ones|onas?)|" +
-            "maric(as?|ón|onas?|ones)|mariquitas?|trolazos?|jotos?|jotol(ón|ones)|jotaz[oa]s?|jototes?|joterías?|amariconad[oa]s?|muerdealmohadas?|mariconaz[oa]s?|jotísim[oa]s?|" +
+            "maric(as?|ón|onas?|ones)|mariquitas?|trolazos?|jotos?|jotol(ón|ones)|jotaz[oa]s?|jototes?|joterías?|amariconad[oa]s?|muerdealmohadas?|mariconaz[oa]s?|jotísim[oa]s?|sodomitas?|" +
             "prostitut[oa]s?|sexoservidor(es|as?)?|rameras?|" +
-            "pornografía|pornográfic[oa]s?|p[o0]rn[o0]s?|z[o0]{2}filia|zoofílic[oa]s?|bestialismo|pedofilia|(sado)?masoquismo|(sado)?masoquistas?|" +
+            "pornografía|pornográfic[oa]s?|p[o0]rn[o0]s?|z[o0]{2}filia|zoofílic[oa]s?|bestialismo|pedofilia|(sado)?masoquismo|(sado)?masoquistas?|sodomías?|" +
             //"|" +
             //"|" +
             ")(?<end>\\s+|[^\\p{IsLatin}]|$)";
