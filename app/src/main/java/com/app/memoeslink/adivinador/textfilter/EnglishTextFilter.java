68a2c919d8204b7a30b8990bd7b5a3886d6806b9
memoeslink <memoeslink@hotmail.com>
2023-05-29 11:27:10 -0600

    Rearranged profanity regex. Added more words to the profanity filter.

--- app/src/main/java/com/app/memoeslink/adivinador/textfilter/EnglishTextFilter.java
+++ app/src/main/java/com/app/memoeslink/adivinador/textfilter/EnglishTextFilter.java
@@ -6,22 +6,39 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EnglishTextFilter implements TextFilter {
-    public static final String CENSOR_REGEX = "\b" + "ass(es)?|arses?|booty|assh[o0]les?|anus(es)?|anal|arseholes?|rect(ums?|a)|rectal|anilingus|" +
-            "tits?|boobs?|breasts?|bosoms?|busty|(tit|breast|ass)(job|fuck)(ers?|ing)?|titt(y|ies)|bazongas?|nipples?|mammar(y|ies)|tittywanks?|titwanks?|tittiefuckers?|" +
-            "sh[i1!]ts?|craps?|golden showers?|(bull|horse|dog)shit|turds?|fae(x|ces)|" +
-            "fag(got)?s?|dykes?|sodomites?|lesbos?|trann(y|ies)|siss(y|ies)|cuntboys?|" +
-            "niggers?|niggas?|wetbacks?|beaners?|nazis?|crackers?|" +
-            "fuck[a-zA-Z]+|fucks?|fuckers?|fucking|fucked|fornicate|fornicators?|fornications?|" +
-            "sex|sexual|copulate|copulations?|orgasms?|genitalia|crotch(es)|" +
-            "dickheads?|suckers?|scum|scumbags?|dumbfucks?|dumbass(es)?|bitch(es)?|bastards?|bollocks?|goddamn(ed)?|kike|pricks?|twats?|wankers?|buggers?|morons?|retards?|shitheads?|motherfuckers?|motherfuckkas?|motherfucking|motherfuck|" +
-            "dicks?|c[o0]cks?|penis(es)?|jizz(es)?|semen|cums?|ejaculations?|ejaculatory|erections?|testicles?|testicular|boners?|choads?|scrot(ums?|a)|cockfoam|dild[o0]s?|ballsacks?|smegma|bukkake|" +
-            "vaginas?|vaginal|vag|puss(y|ies)|clitoris(es)?|cunts?|pubes?|pubic|" +
-            "blowjobs?|(dick|cock)(lick|suck|eat)(ers?|ing)|fellatio|fellations?|masturbate|masturbations?|masturbatory|masterbate|" +
-            "sluts?|hookers?|prostitutes?|whores?|" +
-            "kys|" +
-            "pornography|pornographic|p[o0]rn|p[o0]rn[o0]s?|z[o0]{2}philiac?|bestiality|cp|(sado)?masochism|(sado)?masochists?|(sado)?masochistic|sodom(y|ies)|" +
-            //"|" +
-            //"|" +
+    public static final String COMMON_PROFANITY_REGEX = "(" + "dickheads?|suckers?|scum|scumbags?|dumbfucks?|dumbass(es)?|bitch(es)?|bastards?|bollocks?|goddamn(ed)?|kike|pricks?|twats?|wankers?|buggers?|morons?|retards?|shitheads?|motherfuckers?|motherfuckkas?|motherfucking|motherfuck" + ")";
+    public static final String SEXUAL_INTERCOURSE_PROFANITY_REGEX = "(" + "fuck[a-zA-Z]+|fuckers?|fucking|fucked|fornicate|fornicators?|fornications?|sex|sexual|copulate|copulations?|orgasms?|genitalia|org(y|ies)|blowjobs?|(dick|cock)(lick|suck|eat)(ers?|ing)|fellatio|fellations?" + ")";
+    public static final String BODY_PROFANITY_REGEX = "(" + "ass(es)?|arses?|booty|assh[o0]les?|anus(es)?|anal|arseholes?|rect(ums?|a)|rectal|anilingus|pubes?|pubic|crotch(es)" + ")";
+    public static final String BREASTS_PROFANITY_REGEX = "(" + "tits?|boobs?|breasts?|bosoms?|busty|titt(y|ies)|bazongas?|nipples?|mammar(y|ies)" + ")";
+    public static final String FEMALE_GENITALIA_PROFANITY_REGEX = "(" + "vaginas?|vaginal|vag|puss(y|ies)|clitoris(es)?|cunts?" + ")";
+    public static final String MALE_GENITALIA_PROFANITY_REGEX = "(" + "dicks?|c[o0]cks?|penis(es)?|testicles?|testicular|boners?|choads?|scrot(ums?|a)|cockfoam|ballsacks?|smegma" + ")";
+    public static final String FECAL_MATTER_PROFANITY_REGEX = "(" + "sh[i1!]ts?|craps?|golden showers?|(bull|horse|dog)shit|turds?|fa?eces" + ")";
+    public static final String MASTURBATION_PROFANITY_REGEX = "(" + "masturbate|masturbations?|masturbatory|masterbate|semen|cums?|ejaculations?|jizz(es)?|ejaculatory|erections?" + ")";
+    public static final String HOMOSEXUALITY_PROFANITY_REGEX = "(" + "fag(got)?s?|dykes?|sodomites?|lesbos?|trann(y|ies)|siss(y|ies)|cuntboys?|transexuals?" + ")";
+    public static final String PROSTITUTION_PROFANITY_REGEX = "(" + "sluts?|hookers?|prostitutes?|whores?" + ")";
+    public static final String PORNOGRAPHY_SLURS_REGEX = "(" + "pornography|pornographic|p[o0]rn|p[o0]rn[o0]s?|(tit|breast|ass)(job|fuck)(ers?|ing)?|tittywanks?|titwanks?|tittiefuckers?|sodom(y|ies)|bukkakes?|dild[o0]s?" + ")";
+    public static final String PARAPHILIA_SLURS_REGEX = "(" + "z[o0]{2}philiac?|bestiality|pedophiles?|pedophiliac|ped[o0](es)?|cp|(sado)?masochism|(sado)?masochists?|(sado)?masochistic" + ")";
+    public static final String RACIST_SLURS_REGEX = "(" + "niggers?|niggas?|wetbacks?|beaners?|nazis?|crackers?" + ")";
+    public static final String INTERNET_SLURS_REGEX = "(" + "n[o0]{2}b" + ")";
+    public static final String VIOLENCE_SLURS_REGEX = "(" + "kys" + ")";
+    public static final String CENSOR_REGEX = "\b" +
+            COMMON_PROFANITY_REGEX +
+            "|" + SEXUAL_INTERCOURSE_PROFANITY_REGEX +
+            "|" + BODY_PROFANITY_REGEX +
+            "|" + BREASTS_PROFANITY_REGEX +
+            "|" + FEMALE_GENITALIA_PROFANITY_REGEX +
+            "|" + MALE_GENITALIA_PROFANITY_REGEX +
+            "|" + FECAL_MATTER_PROFANITY_REGEX +
+            "|" + MASTURBATION_PROFANITY_REGEX +
+            "|" + HOMOSEXUALITY_PROFANITY_REGEX +
+            "|" + PROSTITUTION_PROFANITY_REGEX +
+            "|" + PORNOGRAPHY_SLURS_REGEX +
+            "|" + PARAPHILIA_SLURS_REGEX +
+            "|" + RACIST_SLURS_REGEX +
+            "|" + INTERNET_SLURS_REGEX +
+            "|" + VIOLENCE_SLURS_REGEX +
+            //"|" + "" +
+            //"|" + "" +
             "\b";
     public static final Pattern CENSOR_PATTERN = Pattern.compile(CENSOR_REGEX, Pattern.CASE_INSENSITIVE);
 
--- app/src/main/java/com/app/memoeslink/adivinador/textfilter/SpanishTextFilter.java
+++ app/src/main/java/com/app/memoeslink/adivinador/textfilter/SpanishTextFilter.java
@@ -6,21 +6,39 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SpanishTextFilter implements TextFilter {
-    public static final String CENSOR_REGEX = "(?<start>^|\\s+|[^\\p{IsLatin}])(?<text>" + "sexo|sexual(es)?|coitos?|(coital(es)?)|fornicador([ea]s)?|fornicación|fornicatori[oa]s?|orgasmos?|orgásmic[oa]s?|genital(es)?|copular|cópular|copulatori[oa]s?|follar|follatori[oa]s?|follador(es|as?)?|foll(ón|ones|onas?)|follaculos?|" +
-            "t[e3]t[a4]s?|pechos?|senos?|tetamen|tet(ón|ones|onas?)|tetudas?|pez(ón|ones)|tet(ita|ota)s?|pech(ito|ote)s|chich(ita|ota)s?|chich(ón|ones|onas?)|mamar|mamas?|mamari[oa]s?|" +
-            "vaginas?|vaginal(es)?|clítoris|conchud[oa]s?|coños?|coñazos?|panochas?|panoch(ita|ota)s?|panochud[oa]s?|panoch(ón|ones|onas?)|pubis|púbic[oa]s?|" +
-            "p[e3]n[e3]s?|vergas?|verg(uita|ota)s?|vergud[oa]s?|verg(ón|ones)|verguer[oa]s?|vergazos?|pijas?|pijud[oa]s?|testículos?|escrotos?|próstatas?|prostátic[oa]s?|erecci(ón|ones)|pijud[0a]s?|dildos?|prepucios?|esmegma|chotas?|bukkake|" +
-            "nalgas?|nalgonas?|nalg(uita|ota)s?|cul[o0]s?|cul(ón|ones|onas?)|cul(ito|ote)s?|anos?|anal(es)?|rectal(es)?|pedorr[oa]s|pedorrot[oe]s?|" +
-            "mierdas?|mierder[oa]s?|mierd(ón|ones|onas?)|cagadas?|cag(ón|ones|onas?)|cacas?|comemierdas?|he(z|ces)|moj(ón|ones)|" +
-            "semen|eyacular|eyaculaci(ón|ones)|eyaculatori[oa]s?|masturbaci(ón|ones)|masturbatori[oa]s?|onanismos?|puñetas?|puñeter[oa]s?|chaqueter[oa]s?|pajer[oa]s?|" +
-            "malnacid[oa]s?|bastard[oa]s?|pendej[oa]s?|putear|put[oa]s?|puterías?|puteros?|putadas?|putazos?|cabr(ón|onas?|ones)|cabronaz[oa]s?|chingad[oa]s?|jodid[oa]s?|joder|jodederas?|" +
-            "(lame|chupa|mama|muerde)(culo|pija|huevo|bola|verga|polla|pene|escroto|prepucio|nalga|chota)s?|carevergas?|caraculos?|" +
-            "culer[oa]s?|chingar|chingaderas?|chingader(ita|ota)s?|chingonerías?|ching(ón|ones|onas?)|emputad[oa]s?|encabronad[oa]s?|güey(es)?|mam(ón|ones|onas?)|mamadas?|putizas?|pendejear|mamader[oa]s?|put(ón|ones|onas?)|" +
-            "maric(as?|ón|onas?|ones)|mariquitas?|trolazos?|jotos?|jotol(ón|ones)|jotaz[oa]s?|jototes?|joterías?|amariconad[oa]s?|muerdealmohadas?|mariconaz[oa]s?|jotísim[oa]s?|sodomitas?|" +
-            "prostitut[oa]s?|sexoservidor(es|as?)?|rameras?|" +
-            "pornografía|pornográfic[oa]s?|p[o0]rn[o0]s?|z[o0]{2}filia|zoofílic[oa]s?|bestialismo|pedofilia|(sado)?masoquismo|(sado)?masoquistas?|sodomías?|" +
-            //"|" +
-            //"|" +
+    public static final String COMMON_PROFANITY_REGEX = "(" + "malnacid[oa]s?|bastard[oa]s?|pendej[oa]s?|putear|puterías?|puteros?|putadas?|putazos?|cabr(ón|onas?|ones)|cabronaz[oa]s?|chingad[oa]s?|jodid[oa]s?|joder|jodederas?|(lame|chupa|mama|muerde)(culo|pija|huevo|bola|verga|polla|pene|escroto|prepucio|nalga|chota)s?|carevergas?|caraculos?|culer[oa]s?|chingar|chingaderas?|chingader(ita|ota)s?|chingonerías?|ching(ón|ones|onas?)|emputad[oa]s?|encabronad[oa]s?|güey(es)?|mam(ón|ones|onas?)|putizas?|pendejear|mamader[oa]s?" + ")";
+    public static final String SEXUAL_INTERCOURSE_PROFANITY_REGEX = "(" + "sexo|sexual(es)?|coitos?|(coital(es)?)|fornicador([ea]s)?|fornicación|fornicatori[oa]s?|orgasmos?|orgásmic[oa]s?|genital(es)?|copular|cópular|copulatori[oa]s?|follar|follatori[oa]s?|follador(es|as?)?|foll(ón|ones|onas?)|follaculos?|orgías?|mamar|mamadas?" + ")";
+    public static final String BODY_PROFANITY_REGEX = "(" + "nalgas?|nalgonas?|nalg(uita|ota)s?|cul[o0]s?|cul(ón|ones|onas?)|cul(ito|ote)s?|anos?|anal(es)?|rectal(es)?|pedorr[oa]s|pedorrot[oe]s?|pubis|púbic[oa]s?|entrepiernas?" + ")";
+    public static final String BREASTS_PROFANITY_REGEX = "(" + "t[e3]t[a4]s?|pechos?|senos?|tetamen|tet(ón|ones|onas?)|tetudas?|pez(ón|ones)|tet(ita|ota)s?|pech(ito|ote)s|chich(ita|ota)s?|chich(ón|ones|onas?)|mamas?|mamari[oa]s?" + ")";
+    public static final String FEMALE_GENITALIA_PROFANITY_REGEX = "(" + "vaginas?|vaginal(es)?|clítoris|conchud[oa]s?|coños?|coñazos?|panochas?|panoch(ita|ota)s?|panochud[oa]s?|panoch(ón|ones|onas?)" + ")";
+    public static final String MALE_GENITALIA_PROFANITY_REGEX = "(" + "p[e3]n[e3]s?|vergas?|verg(uita|ota)s?|vergud[oa]s?|verg(ón|ones)|verguer[oa]s?|vergazos?|pijas?|pijud[oa]s?|testículos?|escrotos?|próstatas?|prostátic[oa]s?|pijud[0a]s?|prepucios?|esmegma|chotas?" + ")";
+    public static final String FECAL_MATTER_PROFANITY_REGEX = "(" + "mierdas?|mierder[oa]s?|mierd(ón|ones|onas?)|cagadas?|cag(ón|ones|onas?)|cacas?|comemierdas?|he(z|ces)|moj(ón|ones)|soretes?" + ")";
+    public static final String MASTURBATION_PROFANITY_REGEX = "(" + "masturbaci(ón|ones)|masturbatori[oa]s?|onanismos?|puñetas?|puñeter[oa]s?|chaqueter[oa]s?|pajer[oa]s?|semen|eyacular|eyaculaci(ón|ones)|eyaculatori[oa]s?|erecci(ón|ones)" + ")";
+    public static final String HOMOSEXUALITY_PROFANITY_REGEX = "(" + "put[oa]s?|put(ón|ones|onas?)|maric(as?|ón|onas?|ones)|mariquitas?|trolazos?|jotos?|jotol(ón|ones)|jotaz[oa]s?|jototes?|joterías?|amariconad[oa]s?|muerdealmohadas?|mariconaz[oa]s?|jotísim[oa]s?|sodomitas?|transexual(es)?" + ")";
+    public static final String PROSTITUTION_PROFANITY_REGEX = "(" + "prostitut[oa]s?|sexoservidor(es|as?)?|rameras?" + ")";
+    public static final String PORNOGRAPHY_SLURS_REGEX = "(" + "pornografía|pornográfic[oa]s?|p[o0]rn[o0]s?|bukkake|dildos?" + ")";
+    public static final String PARAPHILIA_SLURS_REGEX = "(" + "z[o0]{2}filia|zoofílic[oa]s?|bestialismo|pedófilos?|pedofilia|(sado)?masoquismo|(sado)?masoquistas?|sodomías?" + ")";
+    public static final String RACIST_SLURS_REGEX = "(" + "sudacas?|prietos?" + ")";
+    public static final String INTERNET_SLURS_REGEX = "(" + "n[o0]{2}b" + ")";
+    public static final String VIOLENCE_SLURS_REGEX = "(" + "mátate|suicídate" + ")";
+    public static final String CENSOR_REGEX = "(?<start>^|\\s+|[^\\p{IsLatin}])(?<text>" +
+            COMMON_PROFANITY_REGEX +
+            "|" + SEXUAL_INTERCOURSE_PROFANITY_REGEX +
+            "|" + BODY_PROFANITY_REGEX +
+            "|" + BREASTS_PROFANITY_REGEX +
+            "|" + FEMALE_GENITALIA_PROFANITY_REGEX +
+            "|" + MALE_GENITALIA_PROFANITY_REGEX +
+            "|" + FECAL_MATTER_PROFANITY_REGEX +
+            "|" + MASTURBATION_PROFANITY_REGEX +
+            "|" + HOMOSEXUALITY_PROFANITY_REGEX +
+            "|" + PROSTITUTION_PROFANITY_REGEX +
+            "|" + PORNOGRAPHY_SLURS_REGEX +
+            "|" + PARAPHILIA_SLURS_REGEX +
+            "|" + RACIST_SLURS_REGEX +
+            "|" + INTERNET_SLURS_REGEX +
+            "|" + VIOLENCE_SLURS_REGEX +
+            //"|" + "" +
+            //"|" + "" +
             ")(?<end>\\s+|[^\\p{IsLatin}]|$)";
     public static final Pattern CENSOR_PATTERN = Pattern.compile(CENSOR_REGEX, Pattern.CASE_INSENSITIVE);
 
