51c5374a9743591d24b4331a66633cedd9dd9f39
Memo <memocad@gmail.com>
2023-06-09 23:45:24 -0600

    Added more words to the profanity filter.

--- app/src/main/java/com/app/memoeslink/adivinador/textfilter/EnglishTextFilter.java
+++ app/src/main/java/com/app/memoeslink/adivinador/textfilter/EnglishTextFilter.java
@@ -11,10 +11,10 @@ public class EnglishTextFilter implements TextFilter {
     public static final String VIOLENCE_PROFANITY_REGEX = "(" + "kys" + ")";
     public static final String BODY_PROFANITY_REGEX = "(" + "ass(es)?|arses?|booty|assh[o0]les?|anus(es)?|anal|arseholes?|rect(ums?|a)|rectal|anilingus|pubes?|pubic|crotch(es)|tits?|boobs?|breasts?|bosoms?|busty|titt(y|ies)|bazongas?|nipples?|mammar(y|ies)" + ")";
     public static final String FEMALE_GENITALIA_PROFANITY_REGEX = "(" + "vaginas?|vaginal|vag|puss(y|ies)|clitoris(es)?|cunts?" + ")";
-    public static final String MALE_GENITALIA_PROFANITY_REGEX = "(" + "dicks?|c[o0]cks?|penis(es)?|testicles?|testicular|choads?|scrot(ums?|a)|ballsacks?|smegma" + ")";
+    public static final String MALE_GENITALIA_PROFANITY_REGEX = "(" + "dicks?|c[o0]cks?|penis(es)?|testicles?|testicular|choads?|scrot(ums?|a)|ballsacks?|glan(de)?s|smegma" + ")";
     public static final String MASTURBATION_SLURS_REGEX = "(" + "masturbate|masturbations?|masturbatory|masterbate|fap|onanisms?" + ")";
     public static final String PORNOGRAPHY_SLURS_REGEX = "(" + "pornography|pornographic|p[o0]rn|p[o0]rn[o0]s?|(tit|breast|ass)(job|fuck)(ers?|ing)?|tittywanks?|titwanks?|tittiefuckers?|sodom(y|ies)|bukkakes?|dild[o0]s?|futanari|milfs?|pegging|queening" + ")";
-    public static final String SEXUAL_INTERCOURSE_PROFANITY_REGEX = "(" + "fuck[a-zA-Z]+|fornicate|fornicators?|fornications?|sex|sexual|copulate|copulations?|orgasms?|genitalia|org(y|ies)|blowjobs?|(dick|cock)(lick|suck|eat)(ers?|ing)|fellatio|fellations?|erections?|semen|cums?|ejaculations?|ejaculatory|jizz(es)?|cockfoam|boners?|anilingus|sluts?|hookers?|prostitutes?|whores?" + ")";
+    public static final String SEXUAL_INTERCOURSE_PROFANITY_REGEX = "(" + "fuck[a-z]*|fornicate|fornicators?|fornications?|sex|sexual|copulate|copulations?|orgasms?|genitalia|org(y|ies)|blowjobs?|(dick|cock)(lick|suck|eat)(ers?|ing)|fellatio|fellations?|erections?|semen|cums?|ejaculations?|ejaculatory|jizz(es)?|cockfoam|boners?|anilingus|sluts?|hookers?|prostitutes?|whores?" + ")";
     public static final String FECAL_MATTER_SLURS_REGEX = "(" + "sh[i1!]ts?|shites?|craps?|turds?|fa?eces|fecal" + ")";
     public static final String PARAPHILIA_SLURS_REGEX = "(" + "z[o0]{2}philiac?|bestiality|yiff|pedophiles?|pedophiliac|ped[o0](es)?|cp|(sado)?masochism|(sado)?masochists?|(sado)?masochistic" + ")";
     public static final String HOMOPHOBIC_OR_TRANSPHOBIC_INSULTS_REGEX = "(" + "fag(got)?s?|dykes?|sodomites?|lesbos?|trann(y|ies)|siss(y|ies)|cuntboys?|transexuals?|shemales?" + ")";
