86a3b6913e95e427e1534b46dedca41f2908e2e9
memoeslink <memoeslink@hotmail.com>
2023-06-22 15:49:29 -0600

    Refactored code.

--- app/src/main/java/com/app/memoeslink/adivinador/activity/MainActivity.kt
+++ app/src/main/java/com/app/memoeslink/adivinador/activity/MainActivity.kt
@@ -1170,7 +1170,7 @@ class MainActivity : MenuActivity() {
             status.tasks.add("generateName")
             spnNameType?.isEnabled = false
             val nameType = NameType.values()[spnNameType?.selectedItemPosition ?: 0]
-            tvNameBox?.setText(GeneratorManager(Locale("xx")).nameGenerator.getNameOrRetry(nameType))
+            tvNameBox?.text = GeneratorManager(Locale("xx")).nameGenerator.getNameOrRetry(nameType)
             spnNameType?.isEnabled = true
             status.tasks.remove("generateName")
         }
