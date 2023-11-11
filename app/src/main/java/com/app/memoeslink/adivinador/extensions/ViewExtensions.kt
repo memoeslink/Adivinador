66b0bed72f465972c57c4aaaf9481971f8b48c5c
memoeslink <memoeslink@hotmail.com>
2023-11-11 12:19:19 -0600

    Added fadeIn(...) and fadeOut(...). Refactored fadeAndShow(...).

--- app/src/main/java/com/app/memoeslink/adivinador/extensions/ViewExtensions.kt
+++ app/src/main/java/com/app/memoeslink/adivinador/extensions/ViewExtensions.kt
@@ -2,8 +2,10 @@ package com.app.memoeslink.adivinador.extensions
 
 import android.graphics.Rect
 import android.view.View
+import android.view.animation.AccelerateInterpolator
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
+import android.view.animation.DecelerateInterpolator
 import androidx.core.view.isVisible
 
 fun View?.isObservable(): Boolean {
@@ -12,12 +14,33 @@ fun View?.isObservable(): Boolean {
     return this.getGlobalVisibleRect(rect) && this.height == rect.height() && this.width == rect.width()
 }
 
-fun View?.fadeAndShow() {
+fun View?.fadeIn(offset: Long = 0, duration: Long = 1000) {
     if (this == null) return
     this.clearAnimation()
-    val alphaAnimation = AlphaAnimation(1.0f, 0.0f)
-    alphaAnimation.duration = 350
-    alphaAnimation.repeatCount = 1
-    alphaAnimation.repeatMode = Animation.REVERSE
-    this.startAnimation(alphaAnimation)
+    val animation = AlphaAnimation(0.0f, 1.0f)
+    animation.interpolator = DecelerateInterpolator()
+    animation.startOffset = offset
+    animation.duration = duration
+    this.startAnimation(animation)
+}
+
+fun View?.fadeOut(offset: Long = 0, duration: Long = 1000) {
+    if (this == null) return
+    this.clearAnimation()
+    val animation = AlphaAnimation(1.0f, 0.0f)
+    animation.interpolator = AccelerateInterpolator()
+    animation.startOffset = offset
+    animation.duration = duration
+    this.startAnimation(animation)
+}
+
+fun View?.fadeAndShow(offset: Long = 0, duration: Long = 1000) {
+    if (this == null) return
+    this.clearAnimation()
+    val animation = AlphaAnimation(1.0f, 0.0f)
+    animation.startOffset = offset
+    animation.duration = duration
+    animation.repeatCount = 1
+    animation.repeatMode = Animation.REVERSE
+    this.startAnimation(animation)
 }
